3aaf3d9d11e5e632c4e3a73b82e05d7f36b049b4
TheGrimsey <videostarminer2@gmail.com>
2017-02-24 09:41:11 +0000

    Add blog link

--- NavBar.js
+++ NavBar.js
@@ -2,4 +2,5 @@ document.write('\
 <center><h1>Grimsey Projects and Things</h1>\
 <b>\
 <ul> <li><a href="index.html">Home</a></li>\
-<li><a href="games.html">Games</a></li> </ul></center>');
+<li><a href="games.html">Games</a></li>\
+<li><a href="https://www.thegrimseyblog.wordpress.com">Blog</a></li> </ul></center>');
