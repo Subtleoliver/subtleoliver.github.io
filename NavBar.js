f95c55e117c1ea8102f9b74e6a37542b06f6d8af
Grim <TheGrimsey@users.noreply.github.com>
2021-02-06 22:06:16 +0100

    Update site.

--- NavBar.js
+++
@@ -1,6 +0,0 @@
-document.write('\
-<center><h1>Grimsey Projects and Things</h1>\
-<b>\
-<ul> <li><a href="index.html">Home</a></li>\
-<li><a href="games.html">Games</a></li>\
-<li><a href="https://www.thegrimseyblog.wordpress.com">Blog</a></li> </ul></center>');
--- vars.js
+++
@@ -1,7 +0,0 @@
-var title = "Grimseys Website";
-
-document.title = title;
-
-function setTitle(_title){
-    document.title = title + " : " + _title;
-}
